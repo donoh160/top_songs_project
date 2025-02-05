8b8cecde5c107aefa86fa87ff590d1c32714f8fa
makenna <82896441+makennavick@users.noreply.github.com>
2025-02-04 21:58:06 -0600

    static folder updates

--- static/js/init.js
+++ static/js/init.js
@@ -1,3 +1,7 @@
+// --------------------------------------------------------------------------
+// CREATE YEAR DROPDOWN
+// --------------------------------------------------------------------------
+
 // Flag to track if the dropdown has been changed at least once
 let isFirstChange = true;
 
@@ -12,10 +16,6 @@ document.getElementById('dropdown').addEventListener('change', function() {
         isFirstChange = false;
     }
 });
-
-// --------------------------------------------------------------------------
-// CREATE YEAR DROPDOWN FOR HTML
-// --------------------------------------------------------------------------
 
 // create the year array
 let years = []
@@ -34,7 +34,7 @@ years.forEach((year) => {
 
   
 // --------------------------------------------------------------------------
-// CREATE YEAR DROPDOWN FUNCTION
+// FUNCTION - CURATE/UPDATE PLOTS WHEN DROPDOWN YEAR IS CHOSEN
 // --------------------------------------------------------------------------
 
 // function when an option is chosen from the dropdown
@@ -130,107 +130,6 @@ function duration(targetYear){
 // --------------------------------------------------------------------------
 // ARTIST
 // --------------------------------------------------------------------------
-
-// function artist(targetYear){
-// // Fetch the JSON
-// fetch('../columns_final_df.json')
-//   .then(response => response.json())
-//   .then(data => {
-//     // Do something with the JSON data
-//     console.log(data);
-
-//     //Create array to hold artists
-//     let artistObject = [{'Artist': 'Placeholder', "Frequency": 0}];
-//     let targetYear = 2007;
-
-    
-//     //Create for loop to get all songs release by Artist by year
-//     for (let i = 0; i < Object.keys(data.Artist).length; i++) {
-      
-//       //Check year
-//       let year = data.Year[i]
-//       if (year === targetYear) {
-
-//       //Initialize dummy variable
-//       let located = true
-
-//       // Find artist for each song
-//       let artist = data.artist[i];
-
-//       //Check if artist is already logged
-//       for (let i = 0; i < artistObject.length; i++) {
-      
-//       // Artist found -> Increment by 1
-//       if (artist === artistArray[i]['Artist']) {
-//         artistObject[i]['Frequency'] += 1
-//         break
-//       }
-//       // Artist not found -> Add new artist outside of loop
-//       if (i === (artistObject.length - 1)){
-//         located = false
-//       }
-//     }
-
-//     // Add new artist to object
-//     if (located === false){
-//     artistObject.push({'Artist': artist, 'Frequency': 1})
-//     }
-//   }
-// }
-//   // Sort by decending frequency
-//   artistObject.sort((a, b) => b.Frequency - a.Frequency);
-//   console.log('sorted artistObj', artistObject)
-
-//   // Take all Artists with more than 1 song
-//   artistObject = artistObject.filter(artist => artist.Frequency>1)
-//   console.log('>1 Song', artistObject)
-
-//   // Create arrays for artists and frequencies
-//   let artistArr = []
-//   let freqArr = []
-//   for (let i = 0; i < artistObject.length; i++) {
-//     artistArr.push(artistObject[i]['Artist'])
-//     freqArr.push(artistObject[i]['Frequency'])
-//   }
-//   console.log(artistArr)
-//   console.log(freqArr)
-  
-
-
-//   // Graph Top songs by Artist as bar Graph
-//   let barData = [{
-//     type: "bar",
-//     x: artistArr,
-//     y: freqArr,
-//     // orientation: "h"
-//   }];
-
-//   // Create layout to format graph
-//   let layout = {
-//     title: {
-//       text: `Top Songs by Artist in ${targetYear}`
-//     },
-//     xaxis: {
-//       title: {
-//         text: 'Number of Songs in Top 100'
-//       }
-//     },
-//     yaxis: {
-//       title: {
-//         text: 'Artist'
-//       }
-//     }
-//   };
-
-//   // Plot the graph
-//   Plotly.newPlot('bar', barData, layout);
-// })
-
-// // Handle any errors
-//   .catch(error => {
-//   console.error('Error:', error);
-//   });
-// };
 
 function artist(targetYear){
   fetch('../columns_final_df.json')
@@ -429,86 +328,6 @@ function popularity(targetYear) {
       Plotly.newPlot('chart', dataPlot, layout);
   });
 }
-
-
-
-//   fetch('../columns_final_df.json')
-//       .then(response => response.json())
-//       .then(data => {
-//           let titles = [];
-//           let ranks = [];
-//           let popularities = [];
-
-//           // Loop through data and filter by selected year
-//           Object.keys(data.Year).forEach(index => {
-//               if (data.Year[index] == targetYear) {
-//                   titles.push(data.Title[index]);
-//                   ranks.push(data.Rank[index]);
-//                   popularities.push(data.Popularity[index]);
-//               }
-//           });
-
-//           // Create traces for Rank and Popularity
-//           let trace1 = {
-//               x: titles,
-//               y: popularities,
-//               type: 'bar',
-//               name: 'Popularity',
-//               marker: { color: 'green' },
-//               yaxis: 'y1',
-//               offsetgroup: 1,
-//           };
-
-//           let trace2 = {
-//               x: titles,
-//               y: ranks,
-//               type: 'bar',
-//               name: 'Rank',
-//               marker: { color: 'red', opacity: 0.7 },
-//               yaxis: 'y2',
-//               offsetgroup: 2,
-//           };
-
-//           let layout = {
-//               paper_bgcolor: 'rgba(240,240,240,0.9)',  // Light gray background
-//               plot_bgcolor: 'white',  // White chart background
-//               title: `Song Rankings and Popularity in ${targetYear}`,
-//               width: 1000,
-//               height: 750,
-//               xaxis:{ title: {text: 'Song Title'},
-//               automargin: true,
-//               autosize: false,
-//               margin: {
-//                 l: 50,
-//                 r: 50,
-//                 b: 100,
-//                 t: 100,
-//                 pad: 4
-//               },
-//           },
-//               yaxis: {
-//                   title: 'Popularity',
-//                   side: 'left',
-//                   range: [20,100],
-//                   showgrid: false
-//               },
-//               yaxis2: {
-//                   title: 'Rank',
-//                   overlaying: 'y',
-//                   side: 'right',
-//                   range: [1, 100], // Rank starts at 1 (removes 0)
-//                   tickmode: 'linear', // Keeps ticks evenly spaced
-//                   dtick: 10 // Adjusts tick spacing 
-//               },
-//               barmode: 'group'
-//           };
-
-//           let dataPlot = [trace1, trace2];
-
-//           Plotly.newPlot('chart', dataPlot, layout);
-//       });
-// }
-
 
 // --------------------------------------------------------------------------
 // TOP TEN
